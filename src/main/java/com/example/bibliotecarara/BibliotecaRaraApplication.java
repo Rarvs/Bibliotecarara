76b3d33dfbcc5e2396f568664c01265494ca0893
Rafael Rarvs <rafael.rarvs@gmail.com>
2024-08-21 16:19:16 -0300

    Primeiro - Spring Base

---
+++ src/main/java/com/example/bibliotecarara/BibliotecaRaraApplication.java
@@ -0,0 +1,13 @@
+package com.example.bibliotecarara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaRaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaRaraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bibliotecarara/BibliotecaRaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bibliotecarara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaRaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

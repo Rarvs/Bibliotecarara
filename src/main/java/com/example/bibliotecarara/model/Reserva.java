5381eb7df1c76616026a9732317a6e846b70935a
Caroline Melo <84986038+CarolineFMelo@users.noreply.github.com>
2024-09-04 14:55:27 -0300

    Modifica models de emprestimo, livro e reserva

--- src/main/java/com/example/bibliotecarara/model/Reserva.java
+++ src/main/java/com/example/bibliotecarara/model/Reserva.java
@@ -11,21 +11,33 @@ import java.util.List;
 
 import static jakarta.persistence.CascadeType.ALL;
 
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Reserva {
+public class Reserva extends WithCopy<Reserva> {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "reserva_generator")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Long livroId;
-    private Long usuarioId;
     private Date dataReserva = Date.from(Instant.now());
+    private Long usuarioId;
+    private Long bibliotecaId;
 
-    public Reserva(long livroId, long usuarioId){
+    public Reserva(long livroId, long usuario, long biblioteca){
         this.livroId = livroId;
-        this.usuarioId = usuarioId;
+        this.usuarioId = usuario;
+        this.bibliotecaId = biblioteca;
     }
 
+
+    @Override
+    public void updateWith(Reserva copy) {
+        setId(copy.getId() != null ? copy.getId() : this.getId());
+        setLivroId(copy.getLivroId() != null ? copy.getLivroId() : this.getLivroId());
+        setDataReserva(copy.getDataReserva() != this.getDataReserva() ? copy.getDataReserva() : this.getDataReserva());
+        setUsuarioId(copy.getUsuarioId() != null ? copy.getUsuarioId() : this.getUsuarioId());
+        setBibliotecaId(copy.getBibliotecaId() != null ? copy.getBibliotecaId() : this.getBibliotecaId());
+    }
 }

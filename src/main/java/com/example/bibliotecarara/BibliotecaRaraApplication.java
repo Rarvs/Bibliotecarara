4427f79c9f782c3b984694a84ae3321828ca2e85
Alexandre Rodrigues <85518165+Alexandre2107@users.noreply.github.com>
2024-09-11 11:59:41 -0300

    Fix: BibliotecaRaraApplication.java

--- src/main/java/com/example/bibliotecarara/BibliotecaRaraApplication.java
+++ src/main/java/com/example/bibliotecarara/BibliotecaRaraApplication.java
@@ -2,20 +2,11 @@ package com.example.bibliotecarara;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import javafx.application.Application;
 
 @SpringBootApplication
 public class BibliotecaRaraApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(BibliotecaRaraApplication.class, args);
-
-        // Set the Spring context in the JavaFX application
-        BibliotecaGUI.setApplicationContext(context);
-
-        // Launch JavaFX application
-        Application.launch(BibliotecaGUI.class, args);
+        SpringApplication.run(BibliotecaRaraApplication.class, args);
     }
 }
